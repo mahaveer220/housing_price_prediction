2febccc827ab633243e27891476f30728b5dbafa
mahaveer <37685-mahaveer@users.noreply.gitlab.aicrowd.com>
2025-03-29 13:58:37 +0530

    add home , index, index_json

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import numpy as np
 from flask import Flask, request, render_template
 import joblib
 from logger import Logger
+import jsonify
+import json 
 
 
 import os,sys
@@ -21,6 +23,10 @@ preprocessor= HousingPreprocessor()
 
 @app.route('/')
 def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['GET'])
+def render_index():
     return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
@@ -41,11 +47,46 @@ def predict():
         output = round(prediction[0], 2)
 
         logger.info(f'Predictions run on {input_features}, result: {output}')
-        return render_template('index.html', prediction_text=f'Predicted House Price: ${output}')
+        return render_template('index.html', prediction_text=f'${output}')
     
     except Exception as e:
         logger.error(f"Error in prediction: {e}")
         return render_template('index.html', prediction_text="Error: Invalid Input")
+    
+@app.route('/predict_json', methods=['GET'])
+def render_json_index():
+    return render_template('index_json.html')
+
+@app.route('/predict_json', methods=['POST'])
+def predict_json():
+    try:
+        # Check if the request contains JSON data
+        if request.is_json:
+            data = request.get_json()
+        else:
+            json_input = request.form.get("json_input")  # Get JSON from form input
+            data = json.loads(json_input)  # Convert JSON string to dictionary
+
+        input_features = [data[field] for field in [
+            "MedInc", "HouseAge", "AveRooms", "AveBedrms",
+            "Population", "AveOccup", "Latitude", "Longitude"
+        ]]
+
+        logger.info(input_features)
+        
+        # Convert input into model-friendly format
+        final_features = np.array(input_features)
+        preprocessed_array = preprocessor.preprocess_array(final_features) 
+        
+        # Get prediction
+        prediction = model.predict(preprocessed_array)
+        output = round(prediction[0], 2)
+
+        logger.info(f'Predictions run on {input_features}, result: {output}')
+        return render_template('index_json.html', prediction_text=f'${output}', json_text=json.dumps(data, indent=4))
+    except Exception as e:
+        logger.error(f"Error in prediction: {e}")
+        return render_template('index_json.html', prediction_text="Error: Invalid Input")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
