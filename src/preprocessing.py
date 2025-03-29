0c5502a13f0efe038869af60be2e0b107aa29cd2
mahaveer <37685-mahaveer@users.noreply.gitlab.aicrowd.com>
2025-03-29 20:48:47 +0530

    add pipeline and preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -9,14 +9,15 @@ from sklearn.cluster import KMeans
 from sklearn.base import BaseEstimator, TransformerMixin
 import joblib
 from sklearn.datasets import fetch_california_housing
+
 import os,sys 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from logger import Logger
 
 logger = Logger()
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-PIPELINE_DIR = os.path.join(BASE_DIR,'../pipeline')
-DATA_DIR = os.path.join(BASE_DIR,'../data')
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+PIPELINE_DIR = os.path.join(ROOT_DIR,'../pipeline')
+DATA_DIR = os.path.join(ROOT_DIR,'../data')
 
 class ClusterAdder(BaseEstimator, TransformerMixin):
         def __init__(self, n_clusters=4, lat_col='Latitude', lon_col='Longitude', cluster_col='location_cluster', random_state=42):
@@ -69,26 +70,19 @@ class HousingPreprocessor:
         preprocessed_data = self.pipeline.transform(df)
         return preprocessed_data
 
-    def preprocess_api_json(self, api_json):
-        if self.pipeline is None:
-            if not self.load_pipeline():
-                raise ValueError("Pipeline not loaded. Please train and save the pipeline first.")
-
-        df = pd.DataFrame([api_json])
-        preprocessed_data = self.pipeline.transform(df)
-        return preprocessed_data
 
     def feature_engineering(self, X):
         X['rooms_per_bedroom'] = X['AveRooms'] / X['AveBedrms']
         X['rooms_per_population'] = X['AveRooms'] / X['Population']
         X['population_per_household'] = X['Population'] / X['AveOccup']
         return X
-    
-    
 
     def create_pipeline_and_process_data(self, X_train, X_test, y_train, y_test):
+        logger.info("Creating pipeline and processing data.")
+
         # add cluster
         cluster_adder = ClusterAdder(n_clusters=5)
+        
 
         # feature engineer
         feature_engineer = FunctionTransformer(self.feature_engineering)
@@ -120,47 +114,44 @@ class HousingPreprocessor:
             ('feature_engineer', feature_engineer),
             ('preprocessor', preprocessor)
         ])
+        logger.info("Pipeline created successfully.")
 
         # fit and transform
+        logger.info("Preprocessing the X_train and X_test datasets....")
+        logger.info("Adding Clusters, Feature Engineering and Preprocessing the data w.r.t numerical and cat columns....")
         self.X_train, self.X_test, self.y_train, self.y_test = self.pipeline.fit_transform(X_train, y_train), self.pipeline.transform(X_test), y_train, y_test
         return self.X_train, self.X_test, self.y_train, self.y_test
 
     def save_dataset(self):
+        logger.info("Saving the processed datasets....")
+        if not os.path.exists(DATA_DIR):
+            os.makedirs(DATA_DIR)
         pd.DataFrame(self.X_train).to_csv(self.x_train_path, index=False)
         pd.DataFrame(self.X_test).to_csv(self.x_test_path, index=False)
         pd.DataFrame(self.y_train).to_csv(self.y_train_path, index=False, header=['MedHouseVal'])
         pd.DataFrame(self.y_test).to_csv(self.y_test_path, index=False, header=['MedHouseVal'])
+        logger.info("Processed datasets saved successfully.")
 
 if __name__ == "__main__":
+
     housing = fetch_california_housing(as_frame=True)
+    logger.info("Fetched California housing dataset.")
     data = housing.frame
 
     X = data.drop('MedHouseVal', axis=1)
     y = data['MedHouseVal']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+    logger.info("Splitting the dataset into train and test sets.")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
     preprocessor = HousingPreprocessor()
 
     if not preprocessor.load_pipeline():
         X_train, X_test, y_train, y_test = preprocessor.create_pipeline_and_process_data(X_train, X_test, y_train, y_test)
-        joblib.dump(preprocessor.pipeline, preprocessor.pipeline_path)
-        preprocessor.save_dataset()
 
-        logger.info("Pipeline trained and saved.")
+        joblib.dump(preprocessor.pipeline, preprocessor.pipeline_path)
+        logger.info("Pipeline saved successfully.")
+
+        preprocessor.save_dataset()
     else:
         logger.info("Loaded existing pipeline.")
-
-    # Get original features as dictionary
-    original_features = X.iloc[0].to_dict()  
-    print(f"Original Features: {original_features}")
-
-    # Convert the dict to a list
-    original_features_list = list(original_features.values()) 
-
-    # Use preprocess_array
-    preprocessed_array = preprocessor.preprocess_array([original_features_list]) 
-    print(f'Preprocessed array: {preprocessed_array}')
-
-    api_json = {'AveBedrms': 1.0238095238095237, 'HouseAge': 41.0, 'MedInc': 8.3252,  'AveRooms': 6.984126984126984,  'Population': 322.0, 'AveOccup': 2.5555555555555554, 'Latitude': 37.88, 'Longitude': -122.23}
-    preprocessed_data = preprocessor.preprocess_api_json(api_json)
-    print(f"Preprocessed Data: {preprocessed_data}")
